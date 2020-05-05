c9b56399bc93f2fc36fa23d3916e89e0e42d9fc2
michael <michaelshu@foxmail.com>
2020-05-05 23:02:26 +0800

    [ADD]1、q_chat初版

---
+++ q-chat/src/components/ChatRoom.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="chatRoom">
+    <el-container>
+      <el-aside width="400px">Southbay</el-aside>
+      <el-container>
+        <el-main>
+          <h1>聊天消息</h1>
+          <message-box :messages="messages"></message-box>
+        </el-main>
+        <el-footer>
+          <el-input
+            type="textarea"
+            :rows="5"
+            placeholder="请输入内容"
+            v-model="textarea">
+          </el-input>
+          <el-button type="success" @click="sendMessage">发送消息</el-button>
+        </el-footer>
+      </el-container>
+      <el-aside width="400px">Southbay</el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import MessageBox from './messageBox.vue'
+
+export default {
+  components: {
+    "message-box": MessageBox,
+  },
+  data() {
+    return {
+      textarea: '',
+      messages: [
+        { text: '学习 JavaScript' },
+        { text: '学习 Vue' },
+        { text: '整个牛项目' }
+      ]
+    }
+  },
+  methods: {
+    sendMessage: function () {
+      this.messages.push({text: this.textarea})
+      this.textarea = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ q-chat/src/components/messageBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="message-box" v-scroll-bottom="messages">
+    <li v-for="item in this.messages">
+      {{ item.text }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: Array
+  },
+  data () {
+    return {
+      data: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.message-box{
+  /* position: absolute;
+  height: calc(100% - 6.5rem);
+  width: 100%;
+  margin-top: 3rem; */
+  height: 400px;
+  overflow-y: scroll;
+  background: #1d1b1b;
+  color: aliceblue;
+}
+</style>
